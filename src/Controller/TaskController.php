81c6027b9e02500e1489daa277c02e96a9de1698
Kiriakov Mihail <kiriakovmihail@gmail.com>
2018-02-23 22:00:06 +0200

    Add templates for earch methods, delete test files

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -10,16 +10,20 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
-class Task9Controller extends AbstractController
+class TaskController extends AbstractController
 {
     public function simpleText()
     {
-        return new Response('hello, this\'s simple text !');
+        // return new Response('hello, this\'s simple text !');
+        $text = 'hello, this\'s simple text !!';
+
+        return $this->render('task/simple-text.html.twig', [
+            'text' => $text,
+        ]);
     }
 
     /**
@@ -28,16 +32,25 @@ class Task9Controller extends AbstractController
      */
     public function showTemplates($data)
     {
-        return $this->render('test/test.html.twig', [
+        return $this->render('task/test-template.html.twig', [
             'data' => $data,
         ]);
     }
 
     public function jsonAPI()
     {
-        return new JsonResponse([
+//        return new JsonResponse([
+//            'name' => "Misha",
+//            'age' => 29,
+//        ]);
+
+        $jsonData = [
             'name' => "Misha",
             'age' => 29,
+        ];
+
+        return $this->render('task/json-api.html.twig', [
+            'jsonData' => json_encode($jsonData),
         ]);
     }
 
@@ -50,12 +63,14 @@ class Task9Controller extends AbstractController
 
         $session->set('name', $name);
 
-        return new Response(
-            '<body><body>Name from session (previous value): '. $value .'</body></html>'
-        );
+//        return new Response(
+//            '<html><body>Name from session (previous value): '. $value .'</body></html>'
+//        );
+
+        return $this->render('task/show-name-from-session.html.twig', [
+            'value' => $value,
+        ]);
     }
-
-
 
     public function goToGoogle($page_site)
     {
@@ -75,8 +90,12 @@ class Task9Controller extends AbstractController
      */
     public function getNumber($number)
     {
-        return new Response(
-            '<body><body>Вы ввели число от 1 (defaults) до 99 методом GET: '. $number .'</body></html>'
-        );
+//        return new Response(
+//            '<body><body>Вы ввели число от 1 (defaults) до 99 методом GET: '. $number .'</body></html>'
+//        );
+
+        return $this->render('task/get-number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
